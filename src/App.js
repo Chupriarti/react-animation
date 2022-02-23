53288d08dd67ed362346da12c5d494714892e2a4
Chupriarti <wallx13@gmail.com>
2022-02-23 14:19:35 +0300

    prepare layout

--- src/App.js
+++ src/App.js
@@ -3,7 +3,10 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      React Animation App
+      <button>Show</button>
+      <div className="wrap">
+        <div className="circle"/>
+      </div>
     </div>
   );
 }
