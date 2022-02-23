e45e45a0567efff6e0e16a622aabc8af8d2924a0
Chupriarti <wallx13@gmail.com>
2022-02-23 14:25:39 +0300

    add state control

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
+import React from 'react';
 import './App.css';
 
 function App() {
+  const [loaderVisible, setLoaderVisible] = React.useState(false);
+
   return (
     <div className="App">
-      <button>Show</button>
+      <button onClick={() => setLoaderVisible(!loaderVisible)}>{loaderVisible ? "Hide" : "Show"}</button>
       <div className="wrap">
-        <div className="circle"/>
+        { loaderVisible && <div className="circle"/> }
       </div>
     </div>
   );
