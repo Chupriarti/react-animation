7259b0be8a59548229e0f398eaad9a1794425af3
Chupriarti <wallx13@gmail.com>
2022-02-24 22:07:01 +0300

    test transition events

--- src/App.js
+++ src/App.js
@@ -19,6 +19,12 @@ function App() {
           timeout={500}
           mountOnEnter
           unmountOnExit
+          onEnter={() => console.log("onEnter")}
+          onEntering={() => console.log("onEntering")}
+          onEntered={() => console.log("onEntered")}
+          onExiting={() => console.log("onExiting")}
+          onExited={() => console.log("onExited")}
+          onExit={() => console.log("onExit")}
         >
           {state => <div className={`circle ${state}`}/>}
         </Transition>
