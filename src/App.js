fcde8998407bd504b62ff3ac25491c11a6b732b7
Chupriarti <wallx13@gmail.com>
2022-02-26 23:39:15 +0300

    prepare switch transition

--- src/App.js
+++ src/App.js
@@ -1,20 +1,34 @@
 import React from 'react';
 import './App.css';
-import { CSSTransition } from 'react-transition-group';
+import { CSSTransition, SwitchTransition } from 'react-transition-group';
 
 
 function App() {
   const [mode, setMode] = React.useState("out-in")
   const [toggle, setToggle] = React.useState(false)
 
+  function changeHandler (e) {
+    setMode(e.target.value)
+  }
+
   return (
     <div className="App">
       <div>
         <label htmlFor="out-in">out-in</label>
-        <input id="out-in" value="out-in" name="radio" type="radio" />
+        <input onChange={(e) => changeHandler(e)} id="out-in" value="out-in" name="radio" type="radio" />
         <label htmlFor="in-out">in-out</label>
-        <input id="in-out" value="out-in" name="radio" type="radio" />
+        <input onChange={(e) => changeHandler(e)} id="in-out" value="out-in" name="radio" type="radio" />
       </div>
+      <SwitchTransition mode={mode}>
+        <CSSTransition 
+          timeout={500}
+          classNames="fade"
+        >
+          <button onClick={() => setToggle(!toggle)}>
+            {toggle ? "Hello world" : "Goodbye world"}
+          </button>
+        </CSSTransition>
+      </SwitchTransition>
     </div>
   );
 }
