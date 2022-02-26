c3a33d0b2fe2027bd503c1801478f909c0d9a7b7
Chupriarti <wallx13@gmail.com>
2022-02-26 10:41:39 +0300

    remove old code

--- src/App.js
+++ src/App.js
@@ -4,26 +4,9 @@ import { CSSTransition } from 'react-transition-group';
 
 
 function App() {
-  const [loaderVisible, setLoaderVisible] = React.useState(false);
-
   return (
     <div className="App">
-      <button onClick={() => setLoaderVisible(!loaderVisible)}>{loaderVisible ? "Hide" : "Show"}</button>
-      <div className="wrap">
-        <CSSTransition
-          in={loaderVisible}
-          timeout={500}
-          classNames={{
-            enterActive: 'circle-show',
-            enterDone: 'circle-rotate',
-            exitActive: 'circle-hide',
-           }}
-          mountOnEnter
-          unmountOnExit
-        >
-          <div className='circle'></div>
-        </CSSTransition>
-      </div>
+
     </div>
   );
 }
