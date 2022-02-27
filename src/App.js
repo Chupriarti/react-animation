581c67bc76db84d0a26f99da6d10d75e3954a1ab
Chupriarti <wallx13@gmail.com>
2022-02-27 20:46:46 +0300

    remove old code

--- src/App.js
+++ src/App.js
@@ -1,35 +1,12 @@
 import React from 'react';
 import './App.css';
-import { CSSTransition, SwitchTransition } from 'react-transition-group';
 
 
 function App() {
-  const [mode, setMode] = React.useState("out-in")
-  const [toggle, setToggle] = React.useState(false)
-
-  function changeHandler (e) {
-    setMode(e.target.value)
-  }
 
   return (
     <div className="App">
-      <div>
-        <label htmlFor="out-in">out-in</label>
-        <input onChange={(e) => changeHandler(e)} id="out-in" value="out-in" name="radio" type="radio" />
-        <label htmlFor="in-out">in-out</label>
-        <input onChange={(e) => changeHandler(e)} id="in-out" value="in-out" name="radio" type="radio" />
-      </div>
-      <SwitchTransition mode={mode}>
-        <CSSTransition 
-          key={toggle}
-          timeout={500}
-          classNames="fade"
-        >
-          <button onClick={() => setToggle(!toggle)}>
-            {toggle ? "Hello world" : "Goodbye world"}
-          </button>
-        </CSSTransition>
-      </SwitchTransition>
+
     </div>
   );
 }
