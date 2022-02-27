d44a08c62c35f4a9902b3024e326e81e79d70a94
Chupriarti <wallx13@gmail.com>
2022-02-27 20:55:42 +0300

    prepare layout

--- src/App.js
+++ src/App.js
@@ -6,7 +6,15 @@ function App() {
 
   return (
     <div className="App">
-
+      <div>
+        <input type="text"/>
+        <button>Add item</button>
+      </div>
+      <ul>
+        <li>Start job</li>
+        <li>Do job</li>
+        <li>Finish job</li>
+      </ul>
     </div>
   );
 }
