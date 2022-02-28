5d3b20801bcd282ff4e60c1f16812c79e2475aa3
Chupriarti <wallx13@gmail.com>
2022-02-28 23:44:06 +0300

    prepare routing structure

--- src/App.js
+++ src/App.js
@@ -1,13 +1,33 @@
 import React from 'react';
+import { BrowserRouter, NavLink, Route } from 'react-router-dom';
 import './App.css';
+import About from './pages/About';
+import Main from './pages/Main';
 
 
 function App() {
 
-  return (
-    <div className="App">
+  const routes = [
+    {path: '/', Component: Main},
+    {path: '/about', Component: About}
+  ]
 
-    </div>
+  return (
+    <BrowserRouter>
+      <div className="App">
+        <div className="div">
+          <NavLink to ="/">Main Page</NavLink>
+          <NavLink to ="/about">About Page</NavLink>
+        </div>
+        {routes.map(({path, Component}) => 
+          <Route key={path} exact path={path} >
+            {({match}) => 
+              <Component />
+            }
+          </Route>
+        )}
+      </div>
+    </BrowserRouter>
   );
 }
 
